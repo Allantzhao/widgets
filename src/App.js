b0ab99a8d82a9af815c2e2707caff5184235e723
Tong Allan Zhao <allanzhao@tongs-air.attlocal.net>
2021-09-15 19:16:23 -0700

    Implement accordion using functional components with useState hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Accordion from './Components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end js framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'Because its a favorite amongst front end developers!'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use components'
+  }
+];
 
 export default () => {
-  return <h1>Widgets App</h1>
+  return (
+    <div>
+      <Accordion items={items}/>
+    </div>
+  );
 }
